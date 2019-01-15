be23917a74d9cd8410c5aea7877ae71cb47f814d
lixplus0 <lixplus@qq.com>
2019-01-15 22:45:06 +0800

    2019-1-15 22:45:01

--- NodeJS/main_server.js
+++ NodeJS/main_server.js
@@ -4,6 +4,31 @@ const querystring = require('querystring');
 const urlLib = require('url');
 
 http.createServer(function (req, res) {
+    //GET
+    var obj = urlLib.parse(req.url, true);
+
+    var url = obj.pathname;
+    var GET = obj.query;
+    //POST
+    var str = '';
+    req.on('data', function (data) {
+        str += data;
+    });
+    req.on('end', function () {
+        var POST = querystring.parse(str);
+
+        console.log(url, GET, POST);
+    });
+    
+    var file_name = './www' + url;
+    fs.readFile(file_name, function (err, data) {
+        if (err) {
+            res.write('404');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
 
 
 }).listen(8080);
