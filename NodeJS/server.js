12f053d0ad9b93b14d50889f70b88d33d23d2e94
lixplus0 <lixplus@qq.com>
2019-01-21 20:51:54 +0800

    2019-01-21 20:51:51

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const expressStatic = require('express-static');
+const bodyparser = require('body-parser');
 
 
 var server = express();
 
 server.listen(8080);
 
-server.use(expressStatic('./www'));
+// server.use(bodyparser.urlencoded({
+//     extended: false,
+//     limit: 2 * 1024 * 1024
+// }));
+
+server.use('/', function (req, res,next) {
+    console.log('a');
+    next();
+});
+
+server.use('/', function (req, res, next) {
+    console.log('b');
+});
